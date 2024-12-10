81cef7b432c942c3c0aecb38b4558055f811243f
Caique Sobral <51248056+CaiqueSobral@users.noreply.github.com>
2024-12-09 22:33:02 -0300

    feat: add a new landpage
    * feat: add a new landpage, not finished, but it's work
    
    * refac: change hero texrt
    
    * feat: add download redirect
    
    * feat: tweaks in white background and favicon
    
    * refac: remove useless comma
    
    * refac: abbreviate a phrase for humanize a text
    
    * feat: add a coming in to download button
    
    * refac: change date display
    
    * refac: change requires OS version text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,99 @@
-import {Countdown} from "./countdown";
+/* eslint-disable @next/next/no-img-element */
+import { DetailsCard } from "@/components/composed/details-card/Index";
+import { ArchiveIcon } from "@/assets/icons/Archive";
+import Image from "next/image";
+import { ClockIcon } from "@/assets/icons/Clock";
+import { LightningIcon } from "@/assets/icons/Lightning";
+import { MixerHorizontalIcon } from "@/assets/icons/Mixer";
 
 export default function Home() {
-  return (
-    <div className="w-screen h-screen flex flex-col gap-10 justify-center items-center px-4">
-      <h1 className="text-xl sm:text-2xl md:text-4xl lg:text-6xl uppercase">missed your schedule?</h1>
-      <Countdown />
-    </div>
-  );
+	return (
+		<div className="w-screen flex flex-col items-center justify-center gap-20 mt-28 pb-14">
+			<header>
+				<div className="flex flex-col gap-4 items-center">
+					<Image
+						src="/logo.png"
+						alt="logo"
+						width={100}
+						height={100}
+						className="rounded-3xl"
+					/>
+					<h1 className="text-xl sm:text-2xl md:text-4xl font-semibold text-center text-text-primary">
+						Schedy.io
+					</h1>
+					<h2 className="text-xl sm:text-xl md:text-2xl font-semibold text-text-shade text-center">
+						You work.{" "}
+						<span className="underline">We handle your schedule.</span>
+					</h2>
+				</div>
+			</header>
+			<section className="flex justify-center">
+				<img
+					src="/schedy.png"
+					alt="Schedy at work"
+					className="w-3/4 h-auto rounded-t-3xl block border-b border-b-text-primary"
+				/>
+			</section>
+			<section className="grid grid-cols-2 grid-rows-2 w-3/4 gap-y-10 gap-x-40">
+				<DetailsCard.Root>
+					<DetailsCard.Icon>
+						<ClockIcon />
+					</DetailsCard.Icon>
+					<DetailsCard.Title>Saving Your Time</DetailsCard.Title>
+					<DetailsCard.Description>
+						You don&apos;t need open your calendar to check your schedule,
+						we&apos;ll do it without compromising your space.
+					</DetailsCard.Description>
+				</DetailsCard.Root>
+
+				<DetailsCard.Root>
+					<DetailsCard.Icon>
+						<ArchiveIcon />
+					</DetailsCard.Icon>
+					<DetailsCard.Title>Batteries Included</DetailsCard.Title>
+					<DetailsCard.Description>
+						No need for 10-step configurations, just install it, log in, and you
+						are good to go.
+					</DetailsCard.Description>
+				</DetailsCard.Root>
+
+				<DetailsCard.Root>
+					<DetailsCard.Icon>
+						<LightningIcon />
+					</DetailsCard.Icon>
+					<DetailsCard.Title>Lightweight</DetailsCard.Title>
+					<DetailsCard.Description>
+						Built for the best performance, light and fast.
+					</DetailsCard.Description>
+				</DetailsCard.Root>
+
+				<DetailsCard.Root>
+					<DetailsCard.Icon>
+						<MixerHorizontalIcon />
+					</DetailsCard.Icon>
+					<DetailsCard.Title>Customizable</DetailsCard.Title>
+					<DetailsCard.Description>
+						Customize and use it the way you want.
+					</DetailsCard.Description>
+				</DetailsCard.Root>
+			</section>
+			<section className="flex flex-col items-center justify-center gap-2">
+				<div className="rounded-full flex gap-4 items-center pl-4 bg-text-shade shadow-bright">
+					<span className="text-text-primary font-semibold">Sounds good?</span>
+					<div className="px-6 py-2 bg-text-primary hover:bg-text-primary-shade text-primary rounded-full uppercase text-sm font-bold transition-colors">
+						Coming in <span className="capitalize">dec</span> 23
+						{
+							//Download now
+						}
+					</div>
+				</div>
+				<p className="text-sm text-text-shade">Schedy requires MacOS 14+</p>
+			</section>
+			<aside>
+				<p className="text-text-primary text-sm">
+					&copy; Schedy Team - {new Date().getFullYear()}
+				</p>
+			</aside>
+		</div>
+	);
 }
---
+++ src/components/composed/details-card/components/Description.tsx
@@ -0,0 +1,7 @@
+type Props = {
+	children: string;
+};
+
+export function Description({ children }: Props) {
+	return <span className="text-text-primary-shade">{children}</span>;
+}
