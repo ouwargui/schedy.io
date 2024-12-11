bfd0aa765d0fbc5513e09ed95a1a0dd66ba2023f
Guilherme D'Alessandro <guikemon2@gmail.com>
2024-12-11 03:52:55 -0300

    feat: allow user to download app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,12 +80,12 @@ export default function Home() {
 			<section className="flex flex-col items-center justify-center gap-2">
 				<div className="rounded-full flex gap-4 items-center pl-4 bg-text-shade shadow-bright">
 					<span className="text-text-primary font-semibold">Sounds good?</span>
-					<div className="px-6 py-2 bg-text-primary hover:bg-text-primary-shade text-primary rounded-full uppercase text-sm font-bold transition-colors">
-						Coming in <span className="capitalize">dec</span> 23
-						{
-							//Download now
-						}
-					</div>
+					<a
+						href="http://github.com/ouwargui/schedy/releases/latest/SchedyInstaller.dmg"
+						className="px-6 py-2 bg-text-primary hover:bg-text-primary-shade text-primary rounded-full uppercase text-sm font-bold transition-colors"
+					>
+						Download now
+					</a>
 				</div>
 				<p className="text-sm text-text-shade">Schedy requires MacOS 14+</p>
 			</section>
