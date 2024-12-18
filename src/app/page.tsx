203dd30569224e62d32d2e24b036873d8c2cf4bf
Caique Sobral <caiquelsobral@gmail.com>
2024-12-18 19:54:01 -0300

    refac: refactor schedy image size in big screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="w-full flex flex-col items-center justify-center gap-14 md:gap-20 mt-14 md:mt-28 pb-14 max-w-[1440px] mx-auto">
       <Header />
       <section className="flex justify-center w-full">
-        <div className="w-3/4 h-48 md:h-64 xl:h-80 rounded-t-3xl block border-b border-b-text-primary bg-small_schedy md:bg-schedy bg-cover" />
+        <div className="w-3/4 h-48 md:h-64 lg:h-96 rounded-t-3xl block border-b border-b-text-primary bg-small_schedy md:bg-schedy bg-cover" />
       </section>
       <Details />
       <CTA />
